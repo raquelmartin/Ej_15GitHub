237becbcbbff6ebd21a096a5294eba816f041269
Usuario <Usuario@aula2-PC11>
2015-05-22 11:42:12 +0200

    Refactor DarAltaController-> DarAltaClienteController Refactor DarAltaClienteController->interface

---
+++ src/es/curso/controllers/DarAltaClienteController.java
@@ -0,0 +1,7 @@
+package es.curso.controllers;
+
+import es.curso.model.entity.cliente;
+
+public interface DarAltaClienteController {
+	public void agregar(cliente cliente);
+}
--- src/es/curso/controllers/DarAltaController.java
+++
@@ -1,9 +0,0 @@
-package es.curso.controllers;
-
-import es.curso.model.entity.cliente;
-
-public class DarAltaController {
-	public void agregar(cliente cliente){
-		//programacion.....
-	}
-}
