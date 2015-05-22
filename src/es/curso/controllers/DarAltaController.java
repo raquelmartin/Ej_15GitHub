19c8378dae638be0995596cd6c4faa9e628efbcc
Usuario <Usuario@aula2-PC11>
2015-05-22 09:45:45 +0200

    ADD Controlador DarAltacontroller ADD Entity Cliente

---
+++ src/es/curso/controllers/DarAltaController.java
@@ -0,0 +1,9 @@
+package es.curso.controllers;
+
+import es.curso.model.entity.cliente;
+
+public class DarAltaController {
+	public void agregar(cliente cliente){
+		//programacion.....
+	}
+}
---
+++ src/es/curso/model/entity/cliente.java
@@ -0,0 +1,5 @@
+package es.curso.model.entity;
+
+public class cliente {
+	
+}
